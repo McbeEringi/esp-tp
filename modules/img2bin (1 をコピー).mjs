0cc36881a5292c28815c5fc962ce37e42134334f
McbeEringi <git@mcbeeringi.dev>
2025-12-22 23:06:34 +0900

    -

--- modules/img2bin (1 をコピー).mjs
+++ modules/img2bin (1 をコピー).mjs
@@ -2,7 +2,6 @@ const
 img2bin=({
 	genctx,img,printer_width=384,
 	background_color:bg='#fff',
-	dither_mode=2,
 	transform:{rotate=0,scale=1}={},
 })=>((
 	imgwh=[img.naturalWidth,img.naturalHeight],
@@ -19,11 +18,7 @@ img2bin=({
 	)=>[rwh.map(x=>x*s),s])(),
 	cs=bb.map(Math.ceil),
 	ctx=genctx(...cs),
-	gs,
-	errd=m=>w=>w.forEach((e,i,a)=>(
-		e=e-(a[i]=128<=e?255:0),
-		m.forEach(([dx,dy,d])=>(i+(dy*=cs[0]))<a.length&&(dx+i%cs[0])<cs[0]&&(a[i+dx+dy]+=d*e))
-	))
+	gs
 )=>(
 	ctx.imageSmoothingEnabled=false,
 	ctx.fillStyle=bg,
@@ -31,6 +26,13 @@ img2bin=({
 	ctx.translate(...bb.map(x=>x/2)),
 	ctx.rotate(-rotate),ctx.scale(sc,sc),
 	ctx.drawImage(img,...imgwh.map(x=>x/-2)),
+	// gs=((
+	// 	a=[...ctx.getImageData(0,0,...cs).data],
+	// 	pal=[.299,.587,.114]
+	// )=>[...Array(cs[1])].map((_,y)=>[...Array(cs[0])].map((i,x,{length:w})=>(
+	// 	i=(w*y+x)*4,
+	// 	a.slice(i,i+=3).reduce((a,x,i)=>a+x*pal[i],0)*(a[i]/255)
+	// ))))(),
 	gs=ctx.getImageData(0,0,...cs).data.reduce((a,x,i)=>(
 		i%=4,
 		i==3?(a.a.push(a.x*x/255),a.x=0):(a.x+=x*[.299,.587,.114][i]),
@@ -38,34 +40,30 @@ img2bin=({
 	),{x:0,a:[]}).a,
 
 	// dither
-	
-	[
-		_=>gs=gs.map(x=>128<=x?255:0),
-		_=>gs=gs.map((x,i)=>(
-			([
-				0,8,2,10,
-				12,4,14,6,
-				3,11,1,9,
-				15,7,13,5
-			][(i%cs[0])%4+(Math.floor(i/cs[0])%4)*4]+1)/17*255<x?255:0
-		)),
-		_=>errd([
-			//Floyd-Steinberg
-			[ 1,0,7/16],
-			[-1,1,3/16],[ 0,1,5/16],[ 1,1,1/16]
-		])(gs),
-		_=>errd([
-			//Atkinson
+	// [...Array(cs[1])].forEach((_,y)=>[...Array(cs[0])].forEach((e,x)=>(
+	// 	e=gs[y][x]-(gs[y][x]=128<=gs[y][x]?255:0),
+	// 	[
+	// 		[ 1,0,1/8],[ 2,0,1/8],
+	// 		[-1,1,1/8],[ 0,1,1/8],[ 1,1,1/8],
+	// 		[ 0,2,1/8]
+	// 	].forEach(([dx,dy,d])=>(dy+=y)<cs[1]&&(dx+=x)<cs[0]&&(gs[dy][dx]+=d*e))
+	// ))),
+	gs.forEach((e,i,a)=>(
+		e=e-(a[i]=128<=e?255:0),
+		[
 			[ 1,0,1/8],[ 2,0,1/8],
 			[-1,1,1/8],[ 0,1,1/8],[ 1,1,1/8],
 			[ 0,2,1/8]
-		])(gs)
-	][dither_mode](),
+		].forEach(([dx,dy,d])=>(i+(dy*=cs[0]))<a.length&&(dx+i%cs[0])<cs[0]&&(a[i+dx+dy]+=d*e))
+	)),
 
 	Object.assign([
 		0x1d,0x76,0x30,
 		0,
 		...[Math.ceil(cs[0]/8),cs[1]].flatMap(x=>[x&0xff,x>>8]),
+		// ...gs.flatMap(x=>[...Array(Math.ceil(x.length/8))].map((_,i)=>(
+		// 	x.slice(i*=8,i+8).reduce((a,x,i)=>((a<<1)|(x<128)),0)
+		// )))
 		...gs.reduce((a,x,i)=>(
 			i%=cs[0],
 			a.x|=(x<128)<<(7-i%8),
